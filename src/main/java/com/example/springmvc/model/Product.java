d695e21235e44c45d7293ee47ed39511e782eeb0
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-12-21 10:38:32 +0100

    I begin to develop my Spring MVC Application

---
+++ src/main/java/com/example/springmvc/model/Product.java
@@ -0,0 +1,69 @@
+package com.example.springmvc.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String name;
+    private String description;
+    private String type;
+    private String category;
+    private Double price;
+
+    public Product() {
+    }
+
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
