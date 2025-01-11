688885d7ff35ca592ae3988bd80cb679dfc4a82b
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-01-11 17:46:29 +0100

    DB is initialized random data

---
+++ src/main/java/dev/petproject/dataInit/DataInitializer.java
@@ -0,0 +1,71 @@
+package dev.petproject.dataInit;
+
+import com.github.javafaker.Faker;
+import dev.petproject.domain.Category;
+import dev.petproject.domain.Product;
+import dev.petproject.repository.CategoryRepository;
+import dev.petproject.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.util.StringUtils;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DataInitializer implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+
+    private final Faker faker = new Faker();
+
+    @Value("${spring.app.numberOfCategories}")
+    private int numberOfCategoriesToCreate;
+    @Value("${spring.app.numberOfProducts}")
+    private int numberOfProductsToCreate;
+
+    @Override
+    public void run(String... args) {
+        log.info("Starting data initialization...");
+        initializeData();
+    }
+
+    private void initializeData() {
+        for (int i = 0; i < numberOfCategoriesToCreate; i++) {
+            Category category = createRandomCategory();
+            log.info("Category created: {}", category);
+            categoryRepository.save(category);
+
+
+            for (int j = 0; j < numberOfProductsToCreate; j++) {
+                Product product = createRandomProduct(category);
+                log.info("Product created: {}", product);
+                productRepository.save(product);
+            }
+        }
+        log.info("Database initialized with random categories and products.");
+    }
+
+    private Category createRandomCategory() {
+        String trim = StringUtils.trim("  sfasf ");
+        System.out.println(trim);
+
+        return Category.builder()
+                .name(faker.commerce().department())
+                .build();
+    }
+
+    private Product createRandomProduct(Category category) {
+        String sentence = faker.lorem().sentence(5);
+        return Product.builder()
+                .name(faker.commerce().productName())
+                .price(Double.parseDouble(faker.commerce().price().replace(",", ".")))
+                .quantity(faker.number().numberBetween(1, 100))
+                .description(sentence.length() > 50 ? sentence.substring(0, 50) : sentence)
+                .category(category)
+                .build();
+    }
+}
