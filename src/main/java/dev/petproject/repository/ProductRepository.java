c3f01fcf8af8cde96e258d59a9d018db807e5603
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-02-17 10:01:13 +0100

    I added findByName method

--- src/main/java/dev/petproject/repository/ProductRepository.java
+++ src/main/java/dev/petproject/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package dev.petproject.repository;
 import dev.petproject.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,5 +19,5 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query("SELECT p FROM Product p JOIN FETCH p.category")
     Page<Product> findAllProducts(Pageable pageable);
 
-
+    List<Product> findByName(String name);
 }
